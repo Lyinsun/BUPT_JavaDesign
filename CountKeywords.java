93a130fc4557d73371014efeddc0a512cc158de3
lyinsun <ctdiesgi@qq.com>
2023-12-04 15:18:08 +0800

    feat:12.4号的java程序设计课程, 需要修改文件, 读取的时候不要计算被注释的内容.(已完成)

--- CountKeywords.java
+++ CountKeywords.java
@@ -7,8 +7,9 @@ import java.util.Set;
 public class CountKeywords {
     public static void main(String[] args) throws Exception{
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a Java source file: ");
-        String filename = input.nextLine();
+//        System.out.println("Enter a Java source file: ");
+//        String filename = input.nextLine();
+        String filename = "Ch21Q03_TestFile.java";
 
         File file = new File(filename);
         if(file.exists()){
@@ -37,14 +38,53 @@ public class CountKeywords {
                 new HashSet<>(Arrays.asList(keywordString));
 
         int count = 0;
-        Scanner input = new Scanner(file);
-        while(input.hasNext()){
+        Scanner inputFile = new Scanner(file);
+//        StringBuilder content = new StringBuilder();        //处理后的文本
+        boolean inComment = false;                          //注释标记, 如果在多行注释内的话
+        while (inputFile.hasNextLine()){
+//            System.out.println("inputLine循环");
+            String line = inputFile.nextLine();
+            //处理单行注释
+            int index = line.indexOf("//");
+            if (index != -1){
+                line = line.substring(0,index);
+            }
+
+            //处理内部注释
+            boolean comsModified = false;
+            do {
+//                System.out.println("多注释循环");
+                int comStartIndex = line.indexOf("/*");
+                int comEndIndex = line.indexOf("*/");
+                comsModified = (comStartIndex == -1)&&(comEndIndex == -1);  //同时找到两个才可以
+                if(comStartIndex<comEndIndex){
+                    String lineTemp = line.substring(0,comStartIndex);
+                    if (comEndIndex+1 == line.length()){
+                        line = lineTemp;
+                    }
+                    else {
+                        line = lineTemp + line.substring(comEndIndex+2);
+                    }
+                }
+            } while (!comsModified);
+            String[] tokens = line.split("[\\s\\(\\)\\{\\}]");
+
+            for (int i = 0; i < tokens.length; i++) {
+                if(keywordSet.contains(tokens[i])){
+                    count++;
+                    System.out.print(tokens[i] + " ");
+                }
+            }
+        }
+
+
+/*        while(input.hasNext()){
             String word = input.next();
             if(keywordSet.contains(word)) {
                 count++;
                 System.out.print(word + " ");
             }   
-        }
+        }*/
         System.out.println();
         return count;
     }
