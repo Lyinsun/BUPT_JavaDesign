e6f6b041e0696832901020cd8b297da836480997
lyinsun <ctdiesgi@qq.com>
2023-12-04 08:47:36 +0800

    feat:12.4号的java程序设计课程, 需要修改文件, 读取的时候不要计算被注释的内容.

---
+++ CountKeywords.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class CountKeywords {
+    public static void main(String[] args) throws Exception{
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a Java source file: ");
+        String filename = input.nextLine();
+
+        File file = new File(filename);
+        if(file.exists()){
+            System.out.println("The number of keywords in " + filename
+                + " is " + countKeywords(file));
+        }
+        else{
+            System.out.println("File " + filename + " does not exist");
+        }
+    }
+
+    public static int countKeywords(File file) throws Exception{
+        String[] keywordString = {
+                "abstract", "assert", "boolean", "break",
+                "byte", "case", "catch","char", "class",
+                "const", "extends", "for", "final", "finally",
+                "float", "goto", "if", "implements", "import",
+                "instanceof", "int", "interface", "long",
+                "native", "new", "package", "private", "protected",
+                "public", "return", "short", "static", "strictfp",
+                "super", "switch", "synchronized", "this", "throw",
+                "throws", "transient", "try", "void", "volatile",
+                "while", "true", "false", "null"
+        };
+        Set<String> keywordSet =
+                new HashSet<>(Arrays.asList(keywordString));
+
+        int count = 0;
+        Scanner input = new Scanner(file);
+        while(input.hasNext()){
+            String word = input.next();
+            if(keywordSet.contains(word)) {
+                count++;
+                System.out.print(word + " ");
+            }   
+        }
+        System.out.println();
+        return count;
+    }
+}
