2bc64ac8fe243e3b4d3033591324edc046cafb83
lyinsun <ctdiesgi@qq.com>
2024-06-02 16:33:09 +0800

    fix: 🐛 修复了部分bug，同时整理了gitignore

--- Ch21Q03_TestFile.java
+++ Ch21Q03_TestFile.java
@@ -7,12 +7,16 @@ public class Ch21Q03_TestFile {
         Test t = new Test();
         t.test();
     }
+}
 
-    /* this is a class */
-    void test(){
+/* this is a class */
+class Test {
+    void test() {
         int i;
-        /* this is a class */ int j = 3; /* another comment with keywords static */ int k=3;
-        for(i = 0;i<1000;i++){
+        int j = 3; // 定义并初始化变量 j
+        int k = 3; // 定义并初始化变量 k
+
+        for (i = 0; i < 1000; i++) {
             System.out.println(i);
             i += j;
         }
